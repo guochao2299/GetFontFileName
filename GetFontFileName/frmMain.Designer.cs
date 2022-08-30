d944ea7b8e1259aedba5b420e07f80ef9de613b2
假面骑士龙骑 <332181764@qq.com>
2022-08-30 21:07:37 +0800

    Add files via upload

---
+++ GetFontFileName/frmMain.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace GetFontFileName
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboFontName = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDo
+            // 
+            this.btnDo.Location = new System.Drawing.Point(430, 9);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(94, 29);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "获取文件名";
+            this.btnDo.UseVisualStyleBackColor = true;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择本机已安装字体：";
+            // 
+            // comboFontName
+            // 
+            this.comboFontName.FormattingEnabled = true;
+            this.comboFontName.Location = new System.Drawing.Point(174, 10);
+            this.comboFontName.Name = "comboFontName";
+            this.comboFontName.Size = new System.Drawing.Size(237, 28);
+            this.comboFontName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "字体对应的文件名：";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(174, 52);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(237, 27);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 101);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboFontName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboFontName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileName;
+    }
+}
+
---
+++ GetFontFileName/frmMain.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetFontFileName
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, string> m_fontFileInReg = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboFontName.Items.Clear();
+            comboFontName.Items.Add(string.Empty);
+
+            InstalledFontCollection ifc = new InstalledFontCollection();
+            foreach (FontFamily ff in ifc.Families)
+            {
+                comboFontName.Items.Add(ff.Name);
+            }
+
+            m_fontFileInReg = GetFontFileInfoInReg();
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            if (m_fontFileInReg.ContainsKey(comboFontName.Text))
+            {
+                txtFileName.Text = m_fontFileInReg[comboFontName.Text];
+            }
+            else
+            {
+                txtFileName.Text = GetFontFileName(comboFontName.Text);
+            }
+        }
+
+        public static string GetFontFileName(string fontname)
+        {
+            string folderFullName = System.Environment.GetEnvironmentVariable("windir") + "\\fonts";
+            DirectoryInfo TheFolder = new DirectoryInfo(folderFullName);
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+                if (NextFile.Exists)
+                {
+                    if (fontname == GetFontName(NextFile.FullName))
+                    {
+
+                        return NextFile.Name;
+                    }
+                }
+            }
+            return "";
+        }
+
+        private static string GetFontName(string fontfilename)
+        {
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            //只要ttf和TTF, 其它的本项目不需要
+            if (fontfilename.EndsWith(".ttf") || fontfilename.EndsWith(".TTF") || fontfilename.EndsWith(".ttc"))
+            {
+                pfc.AddFontFile(fontfilename);
+            }
+            if (pfc.Families.Length > 0)
+            {
+                return pfc.Families[0].Name;
+            }
+            return "";
+        }
+
+        private Dictionary<string, string> GetFontFileInfoInReg()
+        {
+            Dictionary<string, string> result= new Dictionary<string, string>();
+
+            RegistryKey localMachineKey = Registry.LocalMachine;  
+            RegistryKey localMachineKeySub = localMachineKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts", false);
+
+            string[] mynames = localMachineKeySub.GetValueNames();
+
+            foreach (string name in mynames)
+            {
+                //获取字体的文件名  
+                string myvalue = localMachineKeySub.GetValue(name).ToString();
+
+                if (myvalue.Substring(myvalue.Length - 4).ToUpper() == ".TTF" && myvalue.Substring(1, 2).ToUpper() != @":")
+                {
+                    string val = name.Substring(0, name.Length - 11);
+                    result[val] = myvalue;
+                }
+            }
+            localMachineKeySub.Close();
+            return result;
+        }
+    }
+}
